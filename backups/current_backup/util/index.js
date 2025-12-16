9c029dcbc69f7a99d3ad52fdb5b59bf1189c9163
sobhihamadi <sobhi.hamadi.12@gmail.com>
2025-12-16 10:21:45 +0200

    Deployment Fix: Permanently untrack and ignore the temporary backups/ directory from the repository history.

--- backups/current_backup/util/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateUniqueId = void 0;
-const uuid_1 = require("uuid");
-const generateUniqueId = (prefix) => {
-    return prefix ? ` ${prefix}_${(0, uuid_1.v4)()}` : (0, uuid_1.v4)();
-};
-exports.generateUniqueId = generateUniqueId;
-//# sourceMappingURL=index.js.map
