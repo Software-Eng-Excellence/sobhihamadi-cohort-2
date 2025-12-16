6132cde8c670f606e1f8924e3a96642e4490f85c
sobhihamadi <sobhi.hamadi.12@gmail.com>
2025-12-16 08:35:36 +0200

    Stop tracking backups directory

--- backups/previous_backup/middleware/AsyncHandler.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.asynchandler = void 0;
-const asynchandler = (fn) => {
-    return (req, res, next) => fn(req, res).catch(next);
-};
-exports.asynchandler = asynchandler;
-//# sourceMappingURL=AsyncHandler.js.map
